7cc4351c4bff7d2cf3fdff5ab6673a1607f23d78
Oleg Lykasov <oleglykasov@gmail.com>
2019-08-02 03:07:25 +0300

    Update

---
+++ precache-manifest.d4717b83157f99cbe2fc047eb858a885.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2e5aecba218ec094ce45f3f0035549",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1c95de2138c008087c2",
+    "url": "/static/css/2.d171d3b2.chunk.css"
+  },
+  {
+    "revision": "48c6d09abccb786fd63e",
+    "url": "/static/css/main.1a773630.chunk.css"
+  },
+  {
+    "revision": "d1c95de2138c008087c2",
+    "url": "/static/js/2.e2add837.chunk.js"
+  },
+  {
+    "revision": "48c6d09abccb786fd63e",
+    "url": "/static/js/main.613fd137.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
---
+++ precache-manifest.ff0995c8d0de649c591d630263e8b93f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c63f6541a6bdaff940d8facb1844dbb",
+    "url": "/revolut-exchange-app/index.html"
+  },
+  {
+    "revision": "f949377c74346b6c6d28",
+    "url": "/revolut-exchange-app/static/css/2.834315fd.chunk.css"
+  },
+  {
+    "revision": "ab2317f5da4379fb9901",
+    "url": "/revolut-exchange-app/static/css/main.1a773630.chunk.css"
+  },
+  {
+    "revision": "f949377c74346b6c6d28",
+    "url": "/revolut-exchange-app/static/js/2.e2add837.chunk.js"
+  },
+  {
+    "revision": "ab2317f5da4379fb9901",
+    "url": "/revolut-exchange-app/static/js/main.4c07b90c.chunk.js"
+  },
+  {
+    "revision": "e056776f697a20fd0981",
+    "url": "/revolut-exchange-app/static/js/runtime~main.135e4bc4.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/revolut-exchange-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/revolut-exchange-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/revolut-exchange-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/revolut-exchange-app/static/media/slick.f97e3bbf.svg"
+  }
+]);
