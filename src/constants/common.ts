ccee6be60dcf76ed1a9e67a1a6bfe789471dd6e8
Oleg Lykasov <oleglykasov@gmail.com>
2019-08-03 17:30:20 +0300

    Change API to more stable one

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,6 +1,6 @@
 import { TCurrency } from '../types';
 
-export const EXCHANGE_API = 'https://api.exchangeratesapi.io';
+export const EXCHANGE_API = 'https://api.ratesapi.io/api';
 export const DEFAULT_CURRENCY_ORDER: Array<TCurrency> = [
   'RUB',
   'USD',
